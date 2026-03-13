18ded75682fce5ffd03d799ee217a6f0caa22bcc
usman51uw <usman51@uwindsor.ca>
2026-03-13 21:52:31 +0000

    Created GameObject, Hero, & Enemy class

--- SpaceShooter.js
+++ SpaceShooter.js
@@ -1,3 +1,11 @@
+function loadTexture(path){
+    return new Promise((resolve) => {
+        const image = new Image;
+        image.src = path;
+        image.onload = () => resolve(image);
+    });
+}
+
 //Canvas setup
 let canvasElement, canvasContext;
 
@@ -7,3 +15,41 @@ window.onload = () => {
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvasElement.width, canvasElement.height);
 };
+
+class GameObject{
+    constructor(x, y, type){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.width = 0;
+        this.height = 0;
+        this.image = null;
+    }
+
+    draw(canvasContext){
+        canvasContext.drawImage(this.image, this.x, this.y, this. width, this. height);
+    }
+}
+
+class Hero extends GameObject{
+    constructor(x, y){
+        super(x, y, 'Hero');
+        this.x = x;
+        this.y = y;
+        this.height= 75;    //height + width come from image size
+        this.width = 99;
+        this.life = 3;
+        this.speed = 8;
+        this.points = 0;
+    }
+}
+
+class Enemy extends GameObject{
+    constructor(x, y){
+        super(x, y, 'Enemy');
+        this.height = 50;   //height + width come from image size
+        this.width = 98;
+        this.speedY = 1;
+
+    }
+}

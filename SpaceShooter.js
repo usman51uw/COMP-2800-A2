0ff546cd09bb6938af1f1cafd55abae69bd5da3c
usman51uw <usman51@uwindsor.ca>
2026-03-13 21:36:06 +0000

    Basic canvas setup in JS

--- SpaceShooter.js
+++ SpaceShooter.js
@@ -1,1 +1,9 @@
 //Canvas setup
+let canvasElement, canvasContext;
+
+window.onload = () => {
+    canvasElement = document.getElementById('canvas');
+    canvasContext = canvasElement.getContext('2d');
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0, 0, canvasElement.width, canvasElement.height);
+};
